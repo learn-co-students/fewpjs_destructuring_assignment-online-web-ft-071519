887fbc0c098bd4f64a37c1947b5064f3da46a7c8
radville <laura.radville@gmail.com>
2019-10-27 13:05:00 -0400

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const farmAnimals = 'cow horse sheep pig chicken'
+const [moo, neigh, baa, oink, cluck ] = farmAnimals.split(" ")
+const [ Bessie, , Dolly, Babe, Little ] = farmAnimals.split(" ")
+const [ black_and_white, , black, pink, ] = farmAnimals.split(" ")
 
 const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+const [red, orange, yellow, green, blue, indigo, violet] = colors
+const [r, o, y, g, b, , v] = colors
+const [ , , , , , indg, ] = colors
 
 const muppet = {
   name: 'Kermit',
@@ -9,6 +15,7 @@ const muppet = {
   job: 'Host of The Muppet Show',
   partner: 'Miss Piggy'
 };
+const {name, color, song, job, partner} = muppet
 
 const k_muppet = {
   k_name: 'Kermit',
@@ -24,6 +31,9 @@ const k_muppet = {
   k_job: 'Host of The Muppet Show',
   k_partner: 'Miss Piggy'
 };
+// 8. Use destructuring to assign songs 2 and 4, and Kermit's job and partner
+const {song_2, song_4} = k_muppet.k_album.the_muppet_movie
+const {k_job, k_partner} = k_muppet
 
 // Strings
 
@@ -46,4 +56,3 @@ const k_muppet = {
 // 7. Use destructuring to assign all appropriate variables using the keys as the variable names
 
 
-// 8. Use destructuring to assign songs 2 and 4, and Kermit's job and partner
